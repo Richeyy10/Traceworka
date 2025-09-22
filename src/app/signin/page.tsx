b1d4d31d9dfa62f8c5f678bed3ca9f199097d8a7
Righteousness <thericheyy@gmail.com>
2025-09-22 06:49:55 +0100

    Update page.tsx

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,6 +1,11 @@
 import { getProviders } from "next-auth/react";
+import { Suspense } from 'react';
 import SigninComponent from "@/components/SigninComponent";
 export default async function SignInPage() {
   const providers = await getProviders();
-  return <SigninComponent providers={providers} />;
+  return(
+    <Suspense fallback={<div>Loading...</div>}>
+      <SigninComponent providers={providers} />;
+    </Suspense>
+  );
 }
